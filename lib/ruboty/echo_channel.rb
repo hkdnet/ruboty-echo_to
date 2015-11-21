2e5238b3f43335bb3ddb4b2d219056141653365f
hkdnet <hkdnet@users.noreply.github.com>
2015-11-21 21:04:56 +0900

    debug

--- lib/ruboty/echo_channel.rb
+++ lib/ruboty/echo_channel.rb
@@ -5,18 +5,21 @@ module Ruboty
   module Handlers
     class EchoChannel < Base
       on(
-        /echo\s+(#.*)\s+(.*)/m,
-        name: 'echo',
+        /echo_to\s+#(.+?)\s+(.*)/m,
+        name: 'echo_to',
         description: 'repeat your message'
       )
 
-      def echo(message)
+      def echo_to(message)
         class << message
           def to
             match_data[1] + @original[:to].split('@')[1]
           end
         end
         message.reply(message.match_data[2])
+        message.reply("to\t#{message.to}")
+        message.reply("match_data[1]\t#{match_data[1]}")
+        message.reply("match_data[2]\t#{match_data[2]}")
       end
     end
   end
